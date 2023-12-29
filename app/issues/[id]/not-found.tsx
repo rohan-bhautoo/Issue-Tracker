14054427d698e0636a81f2d5a88766c6c3b49e8c
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-29 17:51:00 +0400

    Showing Issue Details

---
+++ app/issues/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IssueNotFoundPage = () => {
+  return <div>The requested page cannot be found.</div>;
+};
+
+export default IssueNotFoundPage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -6,8 +6,8 @@ import {
 } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import IssueActions from "./IssueActions";
+import Link from "next/link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -32,7 +32,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
