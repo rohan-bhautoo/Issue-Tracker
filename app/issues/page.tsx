d680d4286bd7f602d59a26a670ecaeadf591e2fc
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-29 20:05:14 +0400

    Styled Link Component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,7 +3,7 @@ import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssueActions from "./IssueActions";
 import delay from "delay";
-import Link from "next/link";
+import Link from "../components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
