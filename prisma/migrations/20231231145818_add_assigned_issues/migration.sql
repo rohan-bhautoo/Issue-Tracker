27081fb0a231bbefb266ff2670d78002fe3f6c9a
Rohan Bhautoo <rohanbhautoo@gmail.com>
2024-01-01 16:56:29 +0400

    Changing relationMode to Prisma for PlanetScale

--- prisma/migrations/20231231145818_add_assigned_issues/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `issue` ADD COLUMN `assignedTo` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedTo_fkey` FOREIGN KEY (`assignedTo`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
